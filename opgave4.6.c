a10bd8207ae5f7cd4c1ae62e834ff247daa2f5ea
Shimitan <43805784+Shimitan@users.noreply.github.com>
2020-10-12 17:00:31 +0200

    4.6 progress

---
+++ opgave4.6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int a,
+        b;
+
+    while (1) {
+        
+        printf("Assign value to a: \n");
+        scanf("%d", &a);
+        printf("Assign value to b: \n");
+        scanf("%d", &b);
+
+        printf("a = %d & b = %d \n", a, b);
+
+        exit;
+
+    }
+    
+    
+    
+
+}
