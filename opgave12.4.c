b4008b7bca4b291bdeae0f4311200fb4ffe52ebd
Shimitan <43805784+Shimitan@users.noreply.github.com>
2020-11-23 17:21:34 +0100

    12.4 done

--- opgave12.4.c
+++ opgave12.4.c
@@ -1,30 +1,38 @@
+/* Mike Jensen - Eksamensopgave 2: Yatzy */
+/* maltow20@student.aau.dk */
+/* A400b Software */
 #include <stdio.h>
 #include <stdlib.h>
 
 #define TOTAL_CARDS 55
 
+/* Laver struct til kulør og pip */
 struct card{
     int suit;
     int pip;
 };
 typedef struct card card;
 
+/* Laver enumerationslite over kulør og pip */
 enum suits{clubs, diamonds, hearts, spades, jokersuit};
 enum pips{two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace, joker};
 
+/* precaller funktioner som prototyper */
 int compare(const void *p1, const void *p2);
 void printfnc(card cardSet[]);
 
+/* Her skabes mit array, hvorefter det sorteres og printes */
 int main(void) {
     
     int j, i, k;
     struct card cardSet[TOTAL_CARDS];
 
+    /* Indsætter jokere i mit array */
     for (j = 0; j < 3; j++){
         cardSet[j].pip = joker;
         cardSet[j].suit = jokersuit;
     }
-    
+    /* Indsætter resterende kort i mit array */
     for (k = spades; k >= clubs; k--){
         for (i = ace; i >= two; i--){
             cardSet[j].pip = i;
@@ -32,7 +40,6 @@ int main(void) {
             j++;
         }
     }
-
     printfnc(cardSet);
     qsort(cardSet, TOTAL_CARDS, sizeof(card), &compare);
     printfnc(cardSet);
@@ -40,25 +47,29 @@ int main(void) {
     return 0;
 }
 
+/* Denne funktion sammenligner 2 værdier */
 int compare(const void *p1, const void *p2){
     card *cp1 = (card*)p1, *cp2 = (card*)p2;
     int result;
     if (cp1->suit < cp2->suit){
     result = -1;
-  } else if (cp1->suit > cp2->suit){
-    result = 1;
-  } else {
-    if (cp1->pip < cp2->pip){
-      result = -1;
-    } else if (cp1->pip > cp2->pip) {
+    } 
+    else if (cp1->suit > cp2->suit){
       result = 1;
-    } else {
-      result = 0;
     }
-  }
+    else {
+      if (cp1->pip < cp2->pip){
+        result = -1;
+      } else if (cp1->pip > cp2->pip) {
+        result = 1;
+      } else {
+        result = 0;
+      }
+    }
     return result;
 }
 
+/* Denne funktion printer alle pip med dens tilhørende kulør */
 void printfnc(card cardSet[]){
     char *suits[] = {"clubs", "diamonds", "hearts", "spades", "jokersuit"}, 
     *pips[] = {"two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace", "joker"};
