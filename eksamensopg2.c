9dc6cbfc50cb834b6746ba9027341e51eb205690
Shimitan <43805784+Shimitan@users.noreply.github.com>
2020-11-25 14:05:18 +0100

    AAAAAAAAAAAAA

--- eksamensopg2.c
+++ eksamensopg2.c
@@ -15,6 +15,11 @@ enum udfald{Ones, Twos, Threes, Fours, Fives, Sixes, onePair, twoPair, threeKind
 /* Programmets funktioner precalles, så programmet ved de er der */
 int* allocation_of_memory(int antalTerninger);
 int roll_multiple_dice(int antalTerninger, int *kast);
+int play_yatzy(int antalTerninger, int *kast, int *score);
+int find_number(int antalTerninger, int *kast, int tal);
+int int_compare(const void *p1, const void *p2);
+int find_pair1(int antalTerninger, int *kast);
+int find_pair2(int antalTerninger, int *kast);
 
 int main(int *terninger, int *kast, int *score){
     
@@ -32,7 +37,7 @@ int main(int *terninger, int *kast, int *score){
         exit(EXIT_FAILURE);
     }
 
-    roll_multiple_dice(antalTerninger, kast);
+    play_yatzy(antalTerninger, kast, score);
 
     free(kast);
     kast = NULL;
@@ -46,12 +51,170 @@ int* allocation_of_memory(int antalTerninger){
 }
 
 int roll_multiple_dice(int antalTerninger, int *kast){
-    //for(int k = 0; k < ANTAL_SPIL; k++){
         for(int i = 0; i < antalTerninger; i++){
             kast[i] = (double)(rand() % 6 + 1);
-            printf("%d ", kast[i]);
         }
-        printf("\n");
-    //}
 }
+
+int play_yatzy(int antalTerninger, int *kast, int *score){
+
+    int i, j, k, temp;
+
+    for (i = Ones; i <= ANTAL_SPIL; i++)
+    {
+        roll_multiple_dice(antalTerninger, kast);
+        switch (i){
+            case Ones:
+                *(score + i) = find_number(antalTerninger, kast, 1);
+                printf("%-12s", "1-ere");
+                for (j = 0; j < antalTerninger; j++){
+                    printf("%d ", *(kast + j));
+                }
+                printf("%6d\n", *(score + i));
+                break;
+            
+            case Twos:
+                *(score + i) = find_number(antalTerninger, kast, 2);
+                printf("%-12s", "2-ere");
+                for (j = 0; j < antalTerninger; j++){
+                    printf("%d ", *(kast + j));
+                }
+                printf("%6d\n", *(score + i));
+                break;
+
+            case Threes:
+                *(score + i) = find_number(antalTerninger, kast, 3);
+                printf("%-12s", "3-ere");
+                for (j = 0; j < antalTerninger; j++){
+                    printf("%d ", *(kast + j));
+                }
+                printf("%6d\n", *(score + i));
+                break;
+
+            case Fours:
+                *(score + i) = find_number(antalTerninger, kast, 4);
+                printf("%-12s", "4-ere");
+                for (j = 0; j < antalTerninger; j++){
+                    printf("%d ", *(kast + j));
+                }
+                printf("%6d\n", *(score + i));
+                break;
+
+            case Fives:
+                *(score + i) = find_number(antalTerninger, kast, 5);
+                printf("%-12s", "5-ere");
+                for (j = 0; j < antalTerninger; j++){
+                    printf("%d ", *(kast + j));
+                }
+                printf("%6d\n", *(score + i));
+                break;
+
+            case Sixes:
+                *(score + i) = find_number(antalTerninger, kast, 6);
+                printf("%-12s", "6-ere");
+                for (j = 0; j < antalTerninger; j++){
+                    printf("%d ", *(kast + j));
+                }
+                printf("%6d\n", *(score + i));
+
+
+                for (j = Ones; j <= Sixes; j++){
+                    temp += *(score + j);
+                }
+                
+                if (temp >= 63){
+                    *(score + i) = 50;
+                    printf("Du får bonus!\n");
+                }
+                else {
+                    *(score + i) = 0;
+                    printf("Du får ingen bonus :(\n");
+                }
+                break;
+
+            case onePair:
+                *(score + i) = find_pair1(antalTerninger, kast);
+                printf("%-12s", "Et par");
+                for (j = 0; j < antalTerninger; j++){
+                    printf("%d ", *(kast + j));
+                }
+                printf("%6d\n", *(score + i));
+                break;
+
+            case twoPair:
+                *(score + i) = find_pair2(antalTerninger, kast);
+                printf("%-12s", "To par");
+                for (j = 0; j < antalTerninger; j++){
+                    printf("%d ", *(kast + j));
+                }
+                printf("%6d\n", *(score + i));
+                break;
+                
+            
+            default:
+                break;
+        }
+    }
+}
+
+
+int find_number(int antalTerninger, int *kast, int tal){
     
+    int score = 0, count = 0;
+
+    for (int i = 0; i <= antalTerninger; i++){
+            if (kast[i] == tal && count <= 4){
+                score++;
+                count++;
+            }
+    }
+    score *= tal;
+    return score;
+}
+
+/* Compare funktion taget fra Kurts powerpoint slide */
+int int_compare(const void *p1, const void *p2){
+    int result, *ip1 = (int *) p1, *ip2 = (int *) p2;
+    if (*ip1 > *ip2){
+        result = -1;
+    }
+    else if (*ip1 < *ip2) {
+        result = 1;
+    }
+    else {
+        result = 0;
+    }
+    return result;
+}
+
+int find_pair1(int antalTerninger, int *kast){
+
+    int score = 0;
+
+
+    qsort(kast, antalTerninger, sizeof(int), &int_compare);
+
+    for (int i = 0; i <= antalTerninger; i++){
+        if (kast[i] == kast[i+1]){
+            score = kast[i]*2;
+            break;
+        }
+    } 
+    return score;
+}
+
+int find_pair2(int antalTerninger, int *kast){
+
+    int score = 0;
+
+
+    qsort(kast, antalTerninger, sizeof(int), &int_compare);
+
+    for (int i = 0; i <= antalTerninger; i++){
+        if (kast[i] == kast[i+1]){
+            score = kast[i]*2;
+            break;
+        }
+    } 
+    return score;
+}
